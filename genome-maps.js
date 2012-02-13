6061c7235b1644b13e5c90b99af9e3b8889f2a00
Francisco Salavert Torres <frasator@gmail.com>
2012-02-13 17:08:31 +0100

    window scrollbars are disabled using style overflow:hidden on body

--- genome-maps.js
+++ genome-maps.js
@@ -51,7 +51,6 @@ function GenomeMaps(targetId,args){
 	
 	//RESIZE EVENT
 	$(window).smartresize(function(a){
-		console.log($(window).width()+'x'+$(window).height());
 		_this.setSize($(window).width(),$(window).height());
 	});
 	
