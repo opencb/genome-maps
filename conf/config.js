50b11635b51183a4e26a69f804d71e1472c866d5
frasator <frasator@gmail.com>
2015-07-10 13:01:44 +0200

    removed old files

--- conf/config.js
+++ conf/config.js
@@ -12,7 +12,8 @@ CELLBASE_HOST = "http://bioinfodev.hpc.cam.ac.uk/cellbase";
 
 // Development
 OPENCGA_VERSION = "v1";
-OPENCGA_HOST = "http://test.babelomics.org/opencga";
+//OPENCGA_HOST = "http://test.babelomics.org/opencga";
+OPENCGA_HOST = "http://bioinfodev.hpc.cam.ac.uk/opencga";
 
 CELLBASE_VERSION = "v3";
 CELLBASE_HOST = "http://bioinfodev.hpc.cam.ac.uk/cellbase";
